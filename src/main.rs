6dca2560a8fd169c2a8088e7c50b348c997b24d6
Tejas013 <tejasmadhukar6@gmail.com>
2024-05-24 13:38:20 +0530

    cli skeleton complete

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::net::SocketAddr;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Play a single player game.
+    Single,
+    /// Play a two player game.
+    Multi {
+        #[command(subcommand)]
+        command: MultiplayerOptions,
+    },
+}
+
+#[derive(Debug, Subcommand)]
+enum MultiplayerOptions {
+    /// Start a server to host game.
+    Host {
+        /// Address to start server.
+        #[arg(short, long, required = true)]
+        address: Option<SocketAddr>,
+    },
+    /// Connect to a server to play.
+    Connect {
+        /// Address to connect client.
+        #[arg(short, long, required = true)]
+        address: Option<SocketAddr>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _args = Args::parse();
 }
